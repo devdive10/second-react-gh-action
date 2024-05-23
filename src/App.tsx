dd77ed2b5b555db8d332149a7ffaca683b109764
mizanmahi <mizanmahi24@gmail.com>
2024-05-23 20:40:22 +0600

    add: some tests

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import PrimaryButton from './components/PrimaryButton';
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world</h1>
+      <Greet />
+      <PrimaryButton />
+    </div>
+  );
+}
+
+export default App;
