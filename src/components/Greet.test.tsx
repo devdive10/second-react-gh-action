01978f6e732dc283290087b632ec014c63c42d4a
mizanmahi <mizanmahi24@gmail.com>
2024-05-24 10:39:45 +0600

    add: wong assertion to a test

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
 describe('Greet()', () => {
-  it('Renders correctly!', () => {
-    // Arrange
-    render(<Greet />);
-    const el = screen.getByText('Hello People!');
-    expect(el).toBeVisible();
-  });
+   it('Renders correctly!', () => {
+      // Arrange
+      render(<Greet />);
+      const el = screen.getByText('Hello People!');
+      expect(el).not.toBeVisible();
+   });
 });
